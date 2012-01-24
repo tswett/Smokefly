1bbe218759684a74314e84fc5d29d1f0472f4be1
swettt@mail.gvsu.edu <swettt@mail.gvsu.edu>
2012-01-23 21:36:48 -0500

    Finished refactoring, so now it works again!

--- Smokefly.py
+++ Smokefly.py
@@ -21,10 +21,12 @@ class Application:
         pygame.init()
         self.screen = pygame.display.set_mode(VIEW_SIZE)
 
-        landscape = session.get_scape
+        landscape = self.session.get_landscape()
         self.port = Viewport(landscape, TILE_WIDTH, TILE_HEIGHT, VIEW_WIDTH, VIEW_HEIGHT)
 
         while 1:
+            player_x, player_y = self.session.get_player()
+            self.port.center_on(player_x, player_y)
             self.port.draw_on(self.screen)
             self.session.tick()
             pygame.time.wait(50)
@@ -55,8 +57,6 @@ class Session:
         self.player_x, self.player_y = 0, 0
 
     def tick(self, force_draw = False):
-        # TODO: move the drawing logic to draw_on()
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
                 # TODO: this doesn't belong here
@@ -75,14 +75,15 @@ class Session:
 
         self.move_player(move_x, move_y)
 
-
-
     def get_landscape(self):
         return self.scape
 
     def move_player(self, dx, dy):
         self.player_x += dx
         self.player_y += dy
+
+    def get_player(self):
+        return self.player_x, self.player_y
 
 class Viewport:
     # My instances represent rectangular regions within Landscapes.
@@ -96,6 +97,7 @@ class Viewport:
         self.tile_width, self.tile_height = tile_width, tile_height
         self.width, self.height = width, height
         self.x, self.y = 0, 0
+            # x and y are the center of the viewport
 
     def get_pos(self):
         return self.x, self.y
@@ -122,13 +124,19 @@ class Viewport:
         return [(x,y) for x in range(coord_left, coord_right+1) for y in range(coord_top, coord_bottom+1)]
 
     def draw_on(self, screen):
-        # TODO: write this
-        tiles = self.port.visible_landscape_squares()
+        tiles = self.visible_landscape_squares()
 
         for (x, y) in tiles:
-            tile_left, tile_top = TILE_WIDTH * x - player_x + (VIEW_WIDTH // 2), TILE_HEIGHT * y - player_y + (VIEW_HEIGHT // 2)
+            tile_left, tile_top = TILE_WIDTH * x - self.x + (VIEW_WIDTH // 2), TILE_HEIGHT * y - self.y + (VIEW_HEIGHT // 2)
             pygame.draw.rect(screen, (0, 255*self.scape.get_lushness((x, y)), 0), (tile_left, tile_top, TILE_WIDTH, TILE_HEIGHT))
 
         pygame.display.update()
+            # It really seems like pygame.display ought to be connected to the screen somehow.
 
-Session()
+    def center_on(self, x, y):
+        self.x, self.y = x, y
+
+#   def get_scape(self):
+#       return self.scape
+
+Application()
